40db57d5706c18aaa51fad2c7f2c570228b0d959
mawenfei <mwf1998@qq.com>
2025-07-22 09:22:15 +0800

    🐞 fix:修改depoy脚本

--- deploy.sh
+++ deploy.sh
@@ -4,26 +4,16 @@
 set -e
 
 # 生成静态文件
-yarn run docs:build
+npm run docs:build
 
-git init
-git add -A
-git commit -m 'deploy'
-
-# 主代码提交到 main 分支（不包括 dist 和 node_modules）
-cd $(dirname "$0")
-echo "dist/" > .gitignore
-echo "node_modules/" >> .gitignore
-git add .
-git commit -m 'update main code'
-git pull origin main --rebase
-git push origin main
-
-# 进入生成的文件夹，部署到 gh-pages 分支
+# 进入生成的文件夹
 cd docs/.vuepress/dist
+
 git init
 git add -A
 git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:mafeiGitHub/afStudyNotes.git master:gh-pages
 
 cd -
